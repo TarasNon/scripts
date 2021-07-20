c10f353b0d0a9b5445ee1b0a3f7adea9bf561942
ghoostpost@gmail.com <ghoostpost@gmail.com>
2021-07-20 21:56:42 +0300

    add files

---
+++ IP.bat
@@ -0,0 +1,3 @@
+XP - netsh interface ip set address name="LAN" source=static addr=192.168.0.33 mask=255.255.255.0
+
+7 - netsh interface ipv4 set address name="Подключение по локальной сети" static 192.168.2.115 255.255.255.0 192.168.2.1 1
